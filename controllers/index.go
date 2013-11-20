a68ed4e3eade97869029da7e7c8582889e331d7c
Joshua <fhbzyc@gmail.com>
2013-11-20 16:15:15 +0800

    Update index.go

--- controllers/index.go
+++ controllers/index.go
@@ -7,7 +7,7 @@ import (
 	"github.com/astaxie/beego"
 	//	"os"
 	//	"path/filepath"
-//	"net/url"
+	"net/url"
 	"strconv"
 	"strings"
 
@@ -29,55 +29,63 @@ type fileType struct {
 
 func (this *IndexController) Get() {
 
-	RequestURI := this.Ctx.Request.RequestURI
+	urlQuery := this.Ctx.Request.RequestURI
+	RequestURI := ""
 
 	DS := "/"
 	//--------------------路径导航--------------------//
 	pathArray := []string{}
 	this.Data["pathArray"] = pathArray
-	//--------------------路径导航--------------------//
-	//	if RequestURI == "" || RequestURI == "/" {
-	//		//		RequestURI = DS + "home"
-	//	}
 
-	filesArray, err := dir.ReadDir(RequestURI)
+	filesArray, err1 := dir.ReadDir(urlQuery)
 
-	if err == nil {
-
-		//结尾加上 '/'
-		//		runeURL := []rune(RequestURI)
-		//		length := len(runeURL)
-		if !strings.HasSuffix(RequestURI, DS) {
-			RequestURI += DS
-		}
-
-		this.Data["parentPath"] = lastDir(RequestURI)
-
-		dirs := []fileType{}
-		files := []fileType{}
-
-		for _, val := range filesArray {
-			time := val.Ftime.Format("2006-01-02 03:04:05")
-			temp := val.Fsys
-			fgname := linux.GetGname(int(temp.Gid))
-			funame := linux.GetUname(int(temp.Uid))
-
-			if val.Ftype == 0 {
-				dirs = append(dirs, fileType{SysFile: val, Fsize: "", Ftime: time, Fgname: fgname, Funame: funame})
+	if err1 != nil {
+		newUrl, err2 := url.QueryUnescape(urlQuery)
+		if err2 != nil {
+			this.Ctx.WriteString("82")
+			return
+		} else {
+			filesArray, err2 = dir.ReadDir(newUrl)
+			if err2 != nil {
+				this.Ctx.WriteString("50")
+				return
 			} else {
-				files = append(files, fileType{SysFile: val, Fsize: strconv.FormatFloat(float64(val.Fsize)/(1024*1024), 'f', 8, 64), Ftime: time, Fgname: fgname, Funame: funame})
+				RequestURI = newUrl
 			}
 		}
-
-		this.Data["RequestURI"] = RequestURI
-		this.Data["dirs"] = dirs
-		this.Data["files"] = files
-		this.TplNames = "index/index.html"
-
 	} else {
-		this.Ctx.WriteString("503")
-		return
+		RequestURI = urlQuery
 	}
+
+	//结尾加上 '/'
+	//		runeURL := []rune(RequestURI)
+	//		length := len(runeURL)
+	if !strings.HasSuffix(RequestURI, DS) {
+		RequestURI += DS
+	}
+
+	this.Data["parentPath"] = lastDir(RequestURI)
+
+	dirs := []fileType{}
+	files := []fileType{}
+
+	for _, val := range filesArray {
+		time := val.Ftime.Format("2006-01-02 03:04:05")
+		temp := val.Fsys
+		fgname := linux.GetGname(int(temp.Gid))
+		funame := linux.GetUname(int(temp.Uid))
+
+		if val.Ftype == 0 {
+			dirs = append(dirs, fileType{SysFile: val, Fsize: "", Ftime: time, Fgname: fgname, Funame: funame})
+		} else {
+			files = append(files, fileType{SysFile: val, Fsize: strconv.FormatFloat(float64(val.Fsize)/(1024*1024), 'f', 8, 64), Ftime: time, Fgname: fgname, Funame: funame})
+		}
+	}
+
+	this.Data["RequestURI"] = RequestURI
+	this.Data["dirs"] = dirs
+	this.Data["files"] = files
+	this.TplNames = "index/index.html"
 
 }
 
